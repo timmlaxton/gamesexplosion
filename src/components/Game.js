be1fef6fbd40d7a77292650e10a79d1136ca50e7
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-09-14 10:21:56 +0100

    rendered upcoming games to home page

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,12 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-const Game = () => {
+const Game = ({ name, released, image, id }) => {
   return (
-    <div>
-      <h3>Game Name</h3>
-      <p>Release Date</p>
-    </div>
+    <StyledGame>
+      <h3>{name}</h3>
+      <p>{released}</p>
+      <img src={image} alt={name} />
+    </StyledGame>
   );
 };
+
+const StyledGame = styled(motion.div)`
+  img {
+    width: 100%;
+  }
+`;
 
 export default Game;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { loadGames } from "../actions/gamesActions";
+import Game from "../components/Game";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -9,7 +12,39 @@ const Home = () => {
     dispatch(loadGames());
   }, [dispatch]);
 
-  return <div></div>;
+  const { popular, newGames, upcoming } = useSelector((state) => state.games);
+
+  return (
+    <GameList>
+      <h1>Upcoming Games</h1>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            id={game.id}
+            image={game.background_image}
+            key={game.id}
+          />
+        ))}
+      </Games>
+    </GameList>
+  );
 };
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 5rem;
+`;
 
 export default Home;
